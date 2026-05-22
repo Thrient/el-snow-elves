f9c97a40156a2fe1320b7db058709986be296406
Thrient <thrient@petalmail.com>
2026-05-22 20:50:59 +0800

    fix: EventSource use HTTPS to avoid mixed content block

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -52,7 +52,7 @@ const MainLayout: FC = () => {
 
   // SSE for real-time update push from hub
   useEffect(() => {
-    const es = new EventSource("http://nas.elarion.cn/api/v1/client/stream");
+    const es = new EventSource("https://nas.elarion.cn/api/v1/client/stream");
     es.onmessage = (e) => {
       try {
         const d = JSON.parse(e.data);
