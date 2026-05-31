480bab457e5fe7f5dec80ee0b751448cc9e08db1
Thrient <thrient@petalmail.com>
2026-05-31 15:37:34 +0800

    Reapply "feat: update blob download to use record_id"
    This reverts commit e9390da92ad3289ae1b4b110db7377ba475ad519.

--- script/core/UpdateWorker.py
+++ script/core/UpdateWorker.py
@@ -53,14 +53,15 @@ class UpdateWorker:
         downloaded_bytes = 0
         for i, item in enumerate(changed):
             save_path = os.path.join(STAGING_DIR, item["path"])
-            _log.info(f"download_updates: [{i + 1}/{total_files}] {item['path']} (id={item['fingerprint_id']}, size={item['size']})")
+            blob_id = item.get("record_id") or item["fingerprint_id"]
+            _log.info(f"download_updates: [{i + 1}/{total_files}] {item['path']} (id={blob_id}, size={item['size']})")
             try:
                 file_bytes_before = downloaded_bytes
 
                 def on_chunk(received: int, _total: int):
                     js.update_progress_bytes(file_bytes_before + received)
 
-                UpdateEngine.download_blob(item["fingerprint_id"], save_path, on_progress=on_chunk)
+                UpdateEngine.download_blob(blob_id, save_path, on_progress=on_chunk)
                 downloaded_bytes += item["size"]
                 _log.info(f"download_updates: {item['path']} done, {downloaded_bytes}/{total_bytes} bytes")
                 js.update_progress(item["path"], i + 1, downloaded_bytes)
